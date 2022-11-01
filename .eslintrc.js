a5c1950ff1974178c8013081919777479a9891eb
bryanfernandesFrwk <bryanfernandes@frwk.com.br>
2022-11-01 10:43:49 -0300

    Keep calm and commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,24 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
+	env: {
+		browser: true,
+		es2021: true,
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"prettier"
-	],
-	"overrides": [
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
-	"plugins": [
-		"react"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+	plugins: ["react"],
+	settings: {
+		react: {
+			version: "18",
+		},
+	},
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
 };
